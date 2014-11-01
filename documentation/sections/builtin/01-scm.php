e9629a884875ef38c527e09c0ce5579d09f36428
Andrés Montañez <andresmontanez@gmail.com>
2014-11-01 17:50:37 -0200

    Add scm/force-update task.

--- documentation/sections/builtin/01-scm.php
+++ documentation/sections/builtin/01-scm.php
@@ -1,9 +1,10 @@
 <h2><a name="builtin-scm"></a>SCM - Software Change Manager</h2>
 <div>
     <p>
-    Currently only <strong>GIT</strong> is supported. The following tasks are available:
-    <ul>
-        <li><strong>scm/update</strong> - Updates the current working copy. Useful as a <em>pre-deploy</em> task for pull all changes before deploying.</li>
-    </ul>
+        Currently only <strong>GIT</strong> is supported. The following tasks are available:
+        <ul>
+            <li><strong>scm/update</strong> - Updates the current working copy. Useful as a <em>pre-deploy</em> task for pull all changes before deploying.</li>
+            <li><strong>scm/force-update</strong> - UDownloads the latest from remote without trying to merge or rebase anything. Resets the master branch to what you just fetched. Changes all the files in your working tree to match the files in origin/master, so if you have any local changes, they will be lost.</li>
+        </ul>
     </p>
 </div>
